d07de99e806269b45522c21c157db591e224355a
Hesusu-ikie <jesuspaezpaez@gmail.com>
2022-01-14 10:11:20 +0100

    Entrega del control check

--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingService.java
@@ -2,21 +2,38 @@ package org.springframework.samples.petclinic.feeding;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.samples.petclinic.pet.Pet;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FeedingService {
+	
+	@Autowired
+	private FeedingRepository fr;
+	
     public List<Feeding> getAll(){
-        return null;
+        return fr.findAll();
     }
 
     public List<FeedingType> getAllFeedingTypes(){
-        return null;
+        return fr.findAllFeedingTypes();
     }
 
     public FeedingType getFeedingType(String typeName) {
-        return null;
+        return fr.findByFeedingType(typeName);
     }
 
-    public Feeding save(Feeding p) throws UnfeasibleFeedingException {
-        return null;       
+    @Transactional(rollbackFor = UnfeasibleFeedingException.class)
+    public Feeding save(Feeding feeding) throws UnfeasibleFeedingException {
+    	Pet p = feeding.getPet();
+    	FeedingType ft = feeding.getFeedingType();
+    	if(!ft.getPetType().equals(p.getType())) {
+    		throw new UnfeasibleFeedingException();
+    	}
+        return fr.save(feeding); 
     }
 
     
--- src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
+++ src/main/java/org/springframework/samples/petclinic/feeding/FeedingType.java
@@ -1,5 +1,15 @@
 package org.springframework.samples.petclinic.feeding;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+import org.springframework.samples.petclinic.model.NamedEntity;
 import org.springframework.samples.petclinic.pet.PetType;
 
 import lombok.Getter;
@@ -7,9 +17,19 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class FeedingType {
-    Integer id;
-    String name;
+@Entity
+public class FeedingType extends BaseEntity{
+	
+	@Size(min = 5, max = 50)
+	@Column(name = "name")
+	@NotNull
+	private String name;
+	
+	@NotNull
     String description;
+	
+	@ManyToOne
+	@NotNull
+	@JoinColumn(name="pet_type_id")
     PetType petType;
 }
